f5d0420c7b1f5294e97e286f29ca41a40b185585
Sandy Syulthon Irfana <64270930+sanstuy@users.noreply.github.com>
2021-10-25 21:55:41 +0700

    Commit dasar tampilan Home perlu diperbaiki dan diperbarui untuk fitur dan tampilannya lagi

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:project_lapang/lapor_screen.dart';
+import 'package:project_lapang/riwayat_laporan.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,49 +13,102 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  CollectionReference laporan =
+      FirebaseFirestore.instance.collection('Laporan');
   CollectionReference user = FirebaseFirestore.instance.collection('users');
 
   late String uid;
+  var isPetugas = false;
+  var isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('UID : $uid'),
-            FutureBuilder<DocumentSnapshot>(
-              future: user.doc(uid).get(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<DocumentSnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return const Text("Something went wrong");
-                }
-
-                if (snapshot.hasData && !snapshot.data!.exists) {
-                  return const Text("Document does not exist");
-                }
-
-                if (snapshot.connectionState == ConnectionState.done) {
-                  Map<String, dynamic> data =
-                      snapshot.data!.data() as Map<String, dynamic>;
-                  return Text(
-                      "Nama: ${data['nama']} \n NIK : ${data['nik']} \n Domisili : ${data['domisili']} \n Nomor Telp : ${data['nomor_hp']}");
-                }
-
-                return const Text("loading");
-              },
-            )
-          ],
-        ),
-      ),
-    );
+        appBar: AppBar(title: const Text('Home')),
+        body: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: ListView(shrinkWrap: true, children: [
+            Container(
+              margin: const EdgeInsets.only(top: 20.0),
+              constraints: const BoxConstraints(
+                  minWidth: 300, maxWidth: double.infinity, minHeight: 80),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(15.0)),
+              child: atas(),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isLoading = true;
+                  });
+                  debugPrint(uid);
+                },
+                child: const Text('isLoading true')),
+            //Synced
+            StreamBuilder<QuerySnapshot>(
+                stream: laporan.where('uid', isEqualTo: uid).snapshots(),
+                builder: (_, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                        children: snapshot.data!.docs
+                            .map((e) => RiwayatLaporan(e['laporan'], e['uid']))
+                            .toList());
+                  } else {
+                    return const Text('Loading');
+                  }
+                }),
+            // Sekali Baca
+            // FutureBuilder<QuerySnapshot>(
+            //     future: laporan.where('uid', isEqualTo: uid).get(),
+            //     builder: (_, snapshot) {
+            //       if (snapshot.hasData) {
+            //         return Column(
+            //             children: snapshot.data!.docs
+            //                 .map((e) => RiwayatLaporan(
+            //                     e['laporan'], e['uid']))
+            //                 .toList());
+            //       } else {
+            //         return Text('Loading');
+            //       }
+            //     })
+            // isLoading ? CircularProgressIndicator() : Text("False")
+          ]),
+        )
+        );
   }
 
   @override
   void initState() {
     super.initState();
     uid = FirebaseAuth.instance.currentUser!.uid;
+  }
+
+  Widget atas() {
+    if (isPetugas) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: const <Widget>[
+          Text('Lihat Laporan'),
+          Text('Buat Laporan'),
+          Text('Tambah Petugas'),
+          Text('Akun')
+        ],
+      );
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        InkWell(
+            child: const Text(
+          'Buat Laporan',
+        ),
+        onTap: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => LaporScreen(uid)));
+        },),
+        const Text('Akun'),
+      ],
+    );
   }
 }
