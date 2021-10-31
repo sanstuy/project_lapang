706c01eddba2d10ed4c463aeeaa384482e327f23
Sandy Syulthon Irfana <64270930+sanstuy@users.noreply.github.com>
2021-10-31 23:12:20 +0700

    Menambahkan fungsi untuk mengambil DocumentID dari laporan

--- lib/riwayat_laporan_screen.dart
+++ lib/riwayat_laporan_screen.dart
@@ -1,24 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class RiwayatLaporanScreen extends StatefulWidget {
   final String uid;
-  final String laporan;
-  const RiwayatLaporanScreen(this.laporan, this.uid, { Key? key }) : super(key: key);
+  final Timestamp tanggal;
+  const RiwayatLaporanScreen(this.tanggal, this.uid, {Key? key})
+      : super(key: key);
 
   @override
   _RiwayatLaporanScreenState createState() => _RiwayatLaporanScreenState();
 }
 
 class _RiwayatLaporanScreenState extends State<RiwayatLaporanScreen> {
+  final CollectionReference laporan =
+      FirebaseFirestore.instance.collection('laporan');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-        Text(widget.laporan),
-        Text(widget.uid)
-        ],
+        body: Center(
+      child: FutureBuilder(
+        future: getDocumentId(),
+        builder: (_, snapshot) {
+          return ElevatedButton(
+              onPressed: () {
+                getDocumentId();
+              },
+              child: Text(snapshot.data.toString()));
+        }
       ),
-    );
+    ));
+  }
+
+  //Mengambil ID dari document yang akan diakses.
+  getDocumentId() async {
+    QuerySnapshot querySnap = await laporan
+        .where('tanggal_laporan', isEqualTo: widget.tanggal)
+        .where('uid', isEqualTo: widget.uid)
+        .get();
+    QueryDocumentSnapshot doc = querySnap.docs[0];
+    DocumentReference docRef = doc.reference;
+    var docId = docRef.id;
+    debugPrint(docId);
+    return docId;
   }
 }
