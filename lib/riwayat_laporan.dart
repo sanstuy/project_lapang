b80d98cb0de04711b2052a8aadd36beac9be411b
Sandy Syulthon Irfana <64270930+sanstuy@users.noreply.github.com>
2021-10-31 23:13:42 +0700

    Menambahkan beberapa variable baru untuk mengcarry data dari screen Home

--- lib/riwayat_laporan.dart
+++ lib/riwayat_laporan.dart
@@ -1,36 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:project_lapang/riwayat_laporan_screen.dart';
 
 class RiwayatLaporan extends StatelessWidget {
+  final String judul_laporan;
   final String laporan;
   final String uid;
-
-  const RiwayatLaporan(this.laporan, this.uid, {Key? key}) : super(key: key);
+  final Timestamp time;
+  const RiwayatLaporan(this.judul_laporan, this.laporan, this.uid, this.time,
+      {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    DateTime tanggal = time.toDate();
+
     return Container(
       width: double.infinity,
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      margin: const EdgeInsets.symmetric(vertical: 10),
       padding: const EdgeInsets.all(5),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8),
           border: Border.all(color: Colors.blue)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
+      child: GestureDetector(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => RiwayatLaporanScreen(time, uid)),
+        ),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: Text(laporan,
-                    style:
-                        const TextStyle(fontWeight: FontWeight.w600, fontSize: 16)),
-              ),
-              Text(uid)
+              Text(judul_laporan,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  )),
+              Text(laporan,
+                  style: const TextStyle(
+                    fontSize: 15,
+                  )),
+              Text(
+                tanggal.toString(),
+                style: const TextStyle(fontSize: 15),
+              )
             ],
           ),
-        ],
+        ),
       ),
     );
   }
