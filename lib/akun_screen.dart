2afe88c08578faf750e5b85df75d9d55e29ca00a
Sandy Syulthon Irfana <64270930+sanstuy@users.noreply.github.com>
2021-11-21 23:53:43 +0700

    Mengubah tampilan

--- lib/akun_screen.dart
+++ lib/akun_screen.dart
@@ -7,7 +7,9 @@ class AkunScreen extends StatefulWidget {
   final String nik;
   final String nomorHp;
   final String uid;
-  const AkunScreen(this.domisili, this.nama, this.nik, this.nomorHp,this.uid, { Key? key }) : super(key: key);
+  const AkunScreen(this.domisili, this.nama, this.nik, this.nomorHp, this.uid,
+      {Key? key})
+      : super(key: key);
 
   @override
   _AkunScreenState createState() => _AkunScreenState();
@@ -15,20 +17,121 @@ class AkunScreen extends StatefulWidget {
 
 class _AkunScreenState extends State<AkunScreen> {
   CollectionReference user = FirebaseFirestore.instance.collection('users');
+  TextEditingController nomorTelp = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-        Text(widget.uid),
-        Text(widget.nik),
-        Text(widget.nama),
-        Text(widget.domisili),
-        Text(widget.nomorHp),
-        ],
+      appBar: AppBar(
+        title: const Text('Akun Anda'),
+        centerTitle: true,
       ),
+      body: ListView(children: [
+        Container(
+          margin: const EdgeInsets.only(top: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'NIK',
+                style: TextStyle(fontSize: 20),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.1,
+                    vertical: MediaQuery.of(context).size.height * 0.01),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                    disabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                    focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue)),
+                    errorBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red)),
+                    hintText: widget.nik,
+                  ),
+                  enabled: false,
+                ),
+              ),
+              const Text(
+                'Nama',
+                style: TextStyle(fontSize: 20),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.1,
+                    vertical: MediaQuery.of(context).size.height * 0.01),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                    disabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                    focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue)),
+                    errorBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red)),
+                    hintText: widget.nama,
+                  ),
+                  enabled: false,
+                ),
+              ),
+              const Text(
+                'Domisili',
+                style: TextStyle(fontSize: 20),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.1,
+                    vertical: MediaQuery.of(context).size.height * 0.01),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                    disabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                    focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue)),
+                    errorBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red)),
+                    hintText: widget.domisili,
+                  ),
+                  enabled: false,
+                ),
+              ),
+              const Text(
+                'Nomor Telepon',
+                style: TextStyle(fontSize: 20),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.1,
+                    vertical: MediaQuery.of(context).size.height * 0.01),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                    focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue)),
+                    errorBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red)),
+                    disabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                    // labelText: 'Nomor Telepon',
+                    hintText: widget.nomorHp,
+                  ),
+                  enabled: false,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 
+  updateNomorTelp() {}
 }
