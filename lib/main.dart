8b34be62591daabe04ca5e4fc24ba9ba895e3d40
sanstuy <sandy330si@gmail.com>
2022-03-11 16:31:33 +0700

    Coba

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sigas',
+      title: 'Sigasa',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         primaryColor: Colors.blue,
